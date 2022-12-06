ca5201d2bbd8953eac211b95c483e9bc67783534
Joanna <astronautnemo@gmail.com>
2022-12-06 13:38:31 +0300

    completed tests 2-4

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email){
+    // Make sure to return the fetch data
+    // Save user information in an object
+    const userInfo = {
+        name: name,
+        email: email,
+    }
+
+    // Valid POST request to http://localhost:3000/users
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(userInfo),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        // Append the data's ID to the DOM
+        const display = document.getElementById('display');
+        const responseId = document.createElement('h1');
+        responseId.textContent = data.id;
+        display.appendChild(responseId);
+    })
+    .catch((object) => {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = object.message;
+        const display = document.getElementById('display');
+        display.appendChild(errorMessage);
+    })
+}
